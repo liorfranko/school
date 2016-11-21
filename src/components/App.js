a9cbb3e057786bbf9f5e509a986b25aed11864be
Alexander Raihelgaus <Alex@Alexanders-MacBook-Pro.local>
2016-11-21 23:52:50 +0200

    some styling, added menu, and shit

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,36 @@
  */
 
 import React from 'react';
+import Menu from './menu/Menu';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      uid: '5826fdc1680d800d2064d1da'
-    }
+      uid: '5826fdc1680d800d2064d1da',
+      data: {}
+    };
+    
+    this.updateState = this.updateState.bind(this);
+  }
+  
+  updateState(data) {
+    this.setState({
+      data: Object.assign({}, this.state.data, data)
+    });
   }
   
   render() {
+    console.log('App.js | this.state', this.state);
+    
     return (
       <div className="container">
         <div>Header - Logo + Menu</div>
-        {this.props.children}
+        <Menu menu={[
+          {name: 'homepage', path: '/'}, {name: 'rests', path: 'rests'}
+          ]}/>
+        {React.Children.map(this.props.children, (child) => React.cloneElement(child, {appData: this.state.data, updateState: this.updateState}))}
         <div>Footer - links, & other shit</div>
       </div>
     )
